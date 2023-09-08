f15932e1758769a4b9e3ca16ed60a03f4ce2f7ab
mario <qiujiafei123@gmail.com>
2023-09-08 22:57:57 +0800

    fix: 暂时消除没有使用的变量告警信息

--- src/envkey.rs
+++ src/envkey.rs
@@ -1,6 +1,9 @@
 pub mod env_key {
+    #[allow(dead_code)]
     pub const MYSQL_URL: &str = "MYSQL_URL";
+    #[allow(dead_code)]
     pub const MYSQL_USER: &str = "MYSQL_USER";
+    #[allow(dead_code)]
     pub const MYSQL_PWD: &str = "MYSQL_PWD";
     pub const APP_ADDR: &str = "APP_ADDR";
 }
